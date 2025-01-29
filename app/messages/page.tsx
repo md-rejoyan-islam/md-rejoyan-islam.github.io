de55ffbf3a92ba7f3c01b4677f1a528074f4b364
md-rejoyan-islam <rejoyanislam0014@gmail.com>
2025-01-30 02:29:55 +0600

    suspense add in message page

--- app/messages/page.tsx
+++ app/messages/page.tsx
@@ -3,6 +3,7 @@ import { ClientMessages } from "@/components/messages/client-messages";
 import { redirect } from "next/navigation";
 
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 export default function MessagesPage() {
   const searchParams = useSearchParams();
   const api_key = searchParams.get("api_key") || "";
@@ -12,13 +13,15 @@ export default function MessagesPage() {
   }
 
   return (
-    <section className="min-h-screen py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-7xl mx-auto">
-        <h1 className="text-4xl font-bold text-center mb-8 text-gray-800 dark:text-gray-100">
-          Client Messages
-        </h1>
-        <ClientMessages />
-      </div>
-    </section>
+    <Suspense>
+      <section className="min-h-screen py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-7xl mx-auto">
+          <h1 className="text-4xl font-bold text-center mb-8 text-gray-800 dark:text-gray-100">
+            Client Messages
+          </h1>
+          <ClientMessages />
+        </div>
+      </section>
+    </Suspense>
   );
 }
